10caabc8b1f3a30aa51be9e9f43d1510b1db8ac6
Bernowly Lima <bernowly@gmail.com>
2016-03-27 22:17:28 -0300

    .

--- app.js
+++ app.js
@@ -2,26 +2,24 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-const redis = require('redis');
-const client = redis.createClient();
-const subscribe = redis.createClient();
+const client = require('redis').createClient();
 
-subscribe.subscribe('pubsub', function(channel, count) {
-    console.log(count  + 'channel(s) subscribed');
+client.subscribe('pubsub', (err, channel) => {
+    console.log(channel + ' channel subscribed');
 });
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', function(socket){
-    subscribe.on("message", function(channel, message) {
+io.on('connection', (socket) => {
+    client.on("message", (channel, message) => {
         var obj = JSON.parse(message);
         socket.emit('msg', obj.message);
     });
 });
 
 var PORT = process.env.PORT || 3000;
-http.listen(PORT, function() {
+http.listen(PORT, () => {
     console.log('listening on : ' + PORT);
 });
